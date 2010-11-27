45ee114f72e1c6f2b0444eebd12e7d34663169f0
Alexandre Gomes <alegomes@gmail.com>
2010-11-27 12:39:41 -0200

    change logic to save last thread dump before a peak. Not based on time anymore

--- catalinastat.sh
+++ catalinastat.sh
@@ -38,7 +38,9 @@ function load() {
         load=$(sysctl vm.loadavg | awk '{print $3}')
      fi
 
-	 # Check if load has increased more than 5 points.
+     #
+	 # Warn the user if load has increased more than 5 points.
+	 #
 	
      # On linux boxes, printf decimal symbol is comma and it doesn't work in [ ] statements
 	 load_diff=$(echo "$load - $last_load" | bc | xargs printf "%1.1f" | sed  s/,/./g)
@@ -49,6 +51,15 @@ function load() {
 		
 	 else
 	    loadincreasing="" 
+	 fi
+	
+	 #
+	 # Save last thread dump if load has increased more than 15 points.
+	 #
+	 if [ $(echo "$load_diff > 15" | bc) -eq 1 ]; then 
+
+		cp $THREADDUMP_TEMP $THREADDUMP_STAT.$(date +%Y%m%d%H%M).JustBeforeLoadPeak.$(hostname).txt
+		
 	 fi
 
      # Networkd conections
@@ -66,52 +77,6 @@ function load() {
      jvm_perm=$(grep -v S0 $JSTAT_TEMP | awk '{print $5}')
      jvm_ygc=$(grep -v S0 $JSTAT_TEMP | awk '{print $6}')
      jvm_fgc=$(grep -v S0 $JSTAT_TEMP | awk '{print $8}')
-
-	 #
-	 # Save last thread dump if a long time has passed since then.
-	 #
-	 # As long as the catalinastat.sh script doesn't run on increasing loads,
-	 # we always face periods without any metric, like the interval
-	 # bellow between 10:44 and 10:46.
-	 #
-	 # Date/Time             Load    AjpEst  AjpTw  MySQLEst  Young   Old     Perm    YGC    FGC  Threads  ThRun  ThBlk  ThTw
-	 # 26/11/2010 10:43:36   0.69   119     0      111       40,33   66,11   98,37   1408   32   598      150    0      448
-	 # 26/11/2010 10:43:50   0.61   120     0      111       38,70   67,45   98,37   1409   32   593      139    0      454
-	 # 26/11/2010 10:44:02   0.52   99      0      111       25,38   72,74   98,37   1410   32   598      151    10     437
-	 # 26/11/2010 10:46:18   69.10   154     0      111       17,67   77,89   98,39   1415   32   1278     363    0      915
-	 # 26/11/2010 10:46:35   49.63   141     0      111       27,94   79,60   98,40   1416   32   1278     322    0      956
-	 # 26/11/2010 10:46:49   38.93   131     0      111       32,84   79,60   98,40   1416   32   1278     312    0      966
-	 #
-	 # To avoid this, we are going to save the last thread dump when 
-	 # a long time has passed since its capture.
-	 # 
-	 # The time arithmetic used is not perfect, but WORKSFORME. 
-	 # Feel free to make it better. ;-)
-	 
-	 if [ ! -z $last_dump_time ]; then
-		current_dump_time=$(echo $now | awk '{print $2}') 
-		
-		min1=$(echo "ibase=10; ${last_dump_time:3:2}" | bc)
-		min2=$(echo "ibase=10; ${current_dump_time:3:2}" | bc) 
-		sec1=$(echo "ibase=10; ${last_dump_time:6:2}" | bc) 
-		sec2=$(echo "ibase=10; ${current_dump_time:6:2}" | bc) 
-	
-		((min_diff = $min2 - $min1)); 
-		((sec_diff = $sec2 - $sec1));  
-
-		# Logica de aritmetica de hora do capeta
-		if [ $min_diff -eq 0 ] && [ $sec_diff -gt 20 ] || 
-		   [ $min_diff -eq 1 ] && [ $sec1 -lt 50  ] || 
-		   [ $min_diff -eq 1 ] && [ $sec2 -gt 20  ] || 
-		   [ $min_diff -gt 1 ]; then 
-
-			# Parece que o ultimo thread dump 
-			# foi instantes antes pico de CPU
-			cp $THREADDUMP_TEMP $THREADDUMP_STAT.$(date +%Y%m%d%H%M).justbefore.$(hostname).txt
-
-		fi
-	
- 	fi
 
      # JVM threads
 
